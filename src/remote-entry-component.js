84ba3a9e84d64d43de5d8c9ab8dc33e4e25f1236
Manish Agrawal <manish.agrawal@freshworks.com>
2025-02-14 19:54:19 +0530

    remote entry tweaked

--- src/remote-entry-component.js
+++ src/remote-entry-component.js
@@ -1,16 +1,53 @@
-import { createApp, defineAsyncComponent } from "vue";
+// import { createApp, defineAsyncComponent } from "vue";
 
+// const componentMap = {
+//     AssetsTable: defineAsyncComponent(() => import("./components/Table.vue")),
+//     AssetDetails: defineAsyncComponent(() => import("./components/AssetDetails.vue")),
+// };
+
+// export default function (container, componentName) {
+//     const Component = componentMap[componentName];
+//     if (!Component) {
+//         console.error(`Component "${componentName}" does not exist.`);
+//         return;
+//     }
+// 	let x = createApp(Component);
+// 	x.mount(container);
+// }
+
+import { createApp, defineComponent, defineAsyncComponent, h } from "vue";
+
+// Define async components
 const componentMap = {
-    AssetsTable: defineAsyncComponent(() => import("./components/Table.vue")),
-    AssetDetails: defineAsyncComponent(() => import("./components/AssetDetails.vue")),
+  AssetsTable: defineAsyncComponent(() => import("./components/Table.vue")),
+  AssetDetails: defineAsyncComponent(() => import("./components/AssetDetails.vue")),
 };
 
-export default function (container, componentName) {
-    const Component = componentMap[componentName];
+// Wrapper component that dynamically renders the requested component
+const WrapperComponent = defineComponent({
+  props: {
+    componentName: String,
+    propsData: Object, // Pass additional props if needed
+  },
+  setup(props) {
+    const Component = componentMap[props.componentName];
+
     if (!Component) {
-        console.error(`Component "${componentName}" does not exist.`);
-        return;
+      console.error(`Component "${props.componentName}" does not exist.`);
+      return () => h("div", {}, `Error: Component "${props.componentName}" not found`);
     }
-	let x = createApp(Component);
-	x.mount(container);
+
+    return () => h(Component, props.propsData);
+  },
+});
+
+// Function to mount Vue component inside the React app
+export default function (container, componentName, propsData = {}) {
+  if (!componentName) {
+    console.error("No component name provided.");
+    return;
+  }
+
+  const app = createApp(WrapperComponent, { componentName, propsData });
+  app.mount(container);
 }
