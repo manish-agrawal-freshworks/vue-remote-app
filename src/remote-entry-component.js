c926ac685a41d6921e61530cad1a33382ac04252
Manish Agrawal <manish.agrawal@freshworks.com>
2025-02-14 20:06:42 +0530

    minor fix

--- src/remote-entry-component.js
+++ src/remote-entry-component.js
@@ -1,23 +1,5 @@
-// import { createApp, defineAsyncComponent } from "vue";
-
-// const componentMap = {
-//     AssetsTable: defineAsyncComponent(() => import("./components/Table.vue")),
-//     AssetDetails: defineAsyncComponent(() => import("./components/AssetDetails.vue")),
-// };
-
-// export default function (container, componentName) {
-//     const Component = componentMap[componentName];
-//     if (!Component) {
-//         console.error(`Component "${componentName}" does not exist.`);
-//         return;
-//     }
-// 	let x = createApp(Component);
-// 	x.mount(container);
-// }
-
 import { createApp, defineComponent, defineAsyncComponent, h } from "vue";
 
-// Define async components
 const componentMap = {
   AssetsTable: defineAsyncComponent(() => import("./components/Table.vue")),
   AssetDetails: defineAsyncComponent(() => import("./components/AssetDetails.vue")),
@@ -31,7 +13,6 @@ const WrapperComponent = defineComponent({
   },
   setup(props) {
     const Component = componentMap[props.componentName];
-
     if (!Component) {
       console.error(`Component "${props.componentName}" does not exist.`);
       return () => h("div", {}, `Error: Component "${props.componentName}" not found`);
